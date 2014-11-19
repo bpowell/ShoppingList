3d5599c1daf541c7156f7fa2135716e581922c83
Rachel Glomski <rrglomsk@oakland.edu>
2014-11-19 15:19:54 -0500

    Add ShoppingItem Class

---
+++ mobile/src/main/java/cse523/oakland/edu/shoppinglist/ShoppingItem.java
@@ -0,0 +1,40 @@
+package cse523.oakland.edu.shoppinglist;
+
+/**
+ * Created by Rachel on 11/19/2014.
+ */
+public class ShoppingItem {
+
+    private String itemName;
+    private int imageId;
+    private boolean purchased;
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+
+    public boolean isPurchased() {
+        return purchased;
+    }
+
+    public void setPurchased(boolean purchased) {
+        this.purchased = purchased;
+    }
+
+
+
+
+
+}
