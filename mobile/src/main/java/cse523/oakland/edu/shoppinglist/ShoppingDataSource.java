7e61e09de8133c3f107a59ef6689b14c0f01092b
Brandon Powell <bpowell29a@gmail.com>
2014-11-25 17:34:52 -0500

    finished getting items from db

--- mobile/src/main/java/cse523/oakland/edu/shoppinglist/ShoppingDataSource.java
+++ mobile/src/main/java/cse523/oakland/edu/shoppinglist/ShoppingDataSource.java
@@ -6,6 +6,8 @@ import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 
+import java.util.ArrayList;
+
 /**
  * Created by brandon on 11/19/14.
  */
@@ -34,7 +36,7 @@ public class ShoppingDataSource {
             ContentValues v = new ContentValues();
             values.put("name", item.getItemName());
             values.put("picid", item.getImageId());
-            values.put("purchase", Integer.valueOf(String.valueOf(item.isPurchased())));
+            values.put("purchased", item.isPurchased());
             long id = database.insert(MySQLiteHelper.TABLE_ITEMS, null, values);
         }
     }
@@ -46,6 +48,19 @@ public class ShoppingDataSource {
         sl.setName(cursor.getString(1));
         cursor.close();
 
+        Cursor cursor1 = database.rawQuery("select * from shopping_items", null);
+        ArrayList<ShoppingItem> si = new ArrayList<ShoppingItem>();
+        cursor1.moveToFirst();
+        while (!cursor1.isAfterLast()) {
+            ShoppingItem i = new ShoppingItem();
+            i.setItemName(cursor1.getString(1));
+            i.setImageId(cursor1.getInt(2));
+            i.setPurchased(cursor1.getInt(3));
+            si.add(i);
+            cursor1.moveToNext();
+        }
+
+        sl.setItems(si);
         return sl;
     }
 }
