802f627ca9a30a502531e252a9c82fa3dc0c1f20
Brandon Powell <bpowell29a@gmail.com>
2014-11-25 18:25:42 -0500

    can send notifications

--- mobile/src/main/java/cse523/oakland/edu/shoppinglist/Main.java
+++ mobile/src/main/java/cse523/oakland/edu/shoppinglist/Main.java
@@ -1,12 +1,19 @@
 package cse523.oakland.edu.shoppinglist;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.logging.Logger;
 
@@ -63,6 +70,45 @@ public class Main extends Activity {
         ds.close();
         Log.d("========", b.getName());
         //end test code
+
+        Button c = (Button)findViewById(R.id.addItem);
+        c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int notificationId = 101;
+                // Build intent for notification content
+                Intent viewIntent = new Intent(getApplicationContext(), SecondActivity.class);
+                PendingIntent viewPendingIntent =
+                        PendingIntent.getActivity(getApplicationContext(), 0, viewIntent, 0);
+
+                //Building notification layout
+                NotificationCompat.Builder notificationBuilder =
+                        new NotificationCompat.Builder(getApplicationContext())
+                                .setSmallIcon(R.drawable.ic_launcher)
+                                .setContentTitle("Demo")
+                                .setContentText("It's demo of simple notification")
+                                .addAction(R.drawable.ic_launcher, "Open The Google", getPendingIntent())
+                                .setContentIntent(viewPendingIntent);
+
+                // instance of the NotificationManager service
+                NotificationManagerCompat notificationManager =
+                        NotificationManagerCompat.from(getApplicationContext());
+
+                // Build the notification and notify it using notification manager.
+                notificationManager.notify(notificationId, notificationBuilder.build());
+            }
+
+            public PendingIntent getPendingIntent(){
+                // Build an intent for an action to open a url
+                Intent urlIntent = new Intent(Intent.ACTION_VIEW);
+                Uri uri = Uri.parse("http://www.google.com");
+                urlIntent.setData(uri);
+                PendingIntent urlPendingIntent =
+                        PendingIntent.getActivity(getApplicationContext(), 0, urlIntent, 0);
+
+                return urlPendingIntent;
+            }
+        });
     }
 
 
